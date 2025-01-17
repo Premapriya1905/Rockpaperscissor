8b0bb6d6638afd3f32bb80c1a90a2c1246f6bf56
Premapriya1905 <138669682+Premapriya1905@users.noreply.github.com>
2025-01-17 13:39:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,84 @@
-const computerChoiceDisplay=document.getElementById("computer-choice")
-const userChoiceDisplay=document.getElementById("user-choice")
-const resultDisplay=document.getElementById("result")
-const possibleChoices = document.querySelectorAll("button")
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const scoreDisplay = document.getElementById("score");
+const restartButton = document.getElementById("restart");
+const possibleChoices = document.querySelectorAll("#buttons button");
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=>{
-    userChoice = e.target.id 
-    userChoiceDisplay.innerHTML = `<img src="aasets/${userChoice}.png" alt="${userChoice}" style="width: 20vw; height: 30vh;">`
-    generateComputerChoice()
-    getResult()
-}))
+let userChoice;
+let computerChoice;
+let result;
+let userScore = 0;
+let computerScore = 0;
 
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random()*3+1)
+possibleChoices.forEach(possibleChoice =>
+    possibleChoice.addEventListener("click", (e) => {
+        userChoice = e.target.id;
+        userChoiceDisplay.innerHTML = `<img src="aasets/${userChoice}.png" alt="${userChoice}" style="width: 20vw; height: 30vh;">`;
+        generateComputerChoice();
+        getResult();
+        updateScore();
+        checkWinner();
+    })
+);
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+
+    if (randomNumber === 1) {
+        computerChoice = "rock";
+    } else if (randomNumber === 2) {
+        computerChoice = "scissor";
+    } else {
+        computerChoice = "paper";
+    }
     
-    if(randomNumber === 1){
-        computerChoice = "rock"
-    }if(randomNumber === 2){
-        computerChoice = "scissor"
-    }if(randomNumber === 3){
-        computerChoice = "paper"
-    }
-    computerChoiceDisplay.innerHTML=`<img src="./aasets/${computerChoice}.png" alt="${computerChoice}" style="width: 20vw; height: 30vh;">`;
+    computerChoiceDisplay.innerHTML = `<img src="aasets/${computerChoice}.png" alt="${computerChoice}" style="width: 20vw; height: 30vh;">`;
 }
 
-function getResult(){
-    if(computerChoice === userChoice){
-        result= "It's a draw!"
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = "It's a tie!";
+    } else if (
+        (computerChoice === "rock" && userChoice === "paper") ||
+        (computerChoice === "scissor" && userChoice === "rock") ||
+        (computerChoice === "paper" && userChoice === "scissor")
+    ) {
+        result = "You win!";
+        userScore++;
+    } else {
+        result = "You lost!";
+        computerScore++;
     }
-    if(computerChoice ==='rock' && userChoice ==='paper'){
-        result= "You win!"
-    }
-    if(computerChoice ==='rock' && userChoice ==='scissor'){
-        result= "You lost!"
-    }
-    if(computerChoice ==='scissor' && userChoice ==='paper'){
-        result= "You lost!"
-    }
-    if(computerChoice ==='scissor' && userChoice ==='rock'){
-        result= "You win!"
-    }
-    if(computerChoice ==='paper' && userChoice ==='rock'){
-        result= "You lost!"
-    }
-    if(computerChoice ==='paper' && userChoice ==='scissor'){
-        result= "You win!"
-    }
-    resultDisplay.innerHTML=result
+    resultDisplay.innerHTML = result;
 }
+
+function updateScore() {
+    scoreDisplay.innerHTML = `${computerScore} - ${userScore}`;
+}
+
+function checkWinner() {
+    if (userScore === 5 || computerScore === 5) {
+        const finalWinner = userScore === 5 ? "User" : "Computer";
+        resultDisplay.innerHTML = `${finalWinner} wins the game!`;
+        disableButtons();
+    }
+}
+
+function disableButtons() {
+    possibleChoices.forEach(button => (button.disabled = true));
+    restartButton.style.display = "block";
+}
+
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    resultDisplay.innerHTML = "";
+    userChoiceDisplay.innerHTML = "";
+    computerChoiceDisplay.innerHTML = "";
+    scoreDisplay.innerHTML = "0 - 0";
+    possibleChoices.forEach(button => (button.disabled = false));
+    restartButton.style.display = "none";
+}
+
+restartButton.addEventListener("click", resetGame);
